b2b8b930749dce5fee8ff070b8e43921929d8cb7
DDihanov <dimitar.dihanov@gmail.com>
2017-03-08 13:32:32 +0200

    Added Boss class, added Pedobear as a boss, added Reader for reading large hitboxes,
    Added Boss class, added Pedobear as a boss, added Reader for reading large hitboxes,

--- src/entities/enemies/bosses/Boss.java
+++ src/entities/enemies/bosses/Boss.java
@@ -1,4 +1,44 @@
-package entities.enemies;
+package entities.enemies.bosses;
 
-public class Boss {
+import entities.GameObject;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.shape.SVGPath;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+public abstract class Boss extends GameObject {
+    private int health;
+    private static HashMap<String, Integer> bosses;
+    private SVGPath svgPath;
+
+    static {
+        bosses = new HashMap<>();
+        bosses.put("Pedobear", 3);
+    }
+
+    public Boss(Canvas canvas, double speed, String imagePath) {
+        super(canvas, speed, imagePath);
+        this.setHealth(bosses.get(this.getClass().getSimpleName()));
+    }
+
+    protected abstract void initializeHitbox() throws FileNotFoundException;
+
+    public SVGPath getSvgPath() {
+        return svgPath;
+    }
+
+    protected void setSvgPath(SVGPath svgPath) {
+        this.svgPath = svgPath;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    protected void setHealth(int health) {
+        this.health = health;
+    }
+
+    public abstract void move();
 }
--- src/entities/enemies/bosses/Pedobear.java
+++ src/entities/enemies/bosses/Pedobear.java
@@ -1,4 +1,71 @@
 package entities.enemies.bosses;
 
-public class Pedobear {
+import entities.Constants;
+import entities.SpawnCoordinates;
+import helpers.Reader;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.shape.SVGPath;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Timer;
+
+public class Pedobear extends Boss {
+    private double randomY;
+    private Timer timer;
+
+    public Pedobear(Canvas canvas, double speed) {
+        super(canvas, speed, Constants.BOSS_PEDOBEAR);
+        try {
+            this.initializeHitbox();
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+        this.timer = new Timer();
+        this.setRandomY(SpawnCoordinates.getRandom(720));
+    }
+
+    @Override
+    protected void initializeHitbox() throws FileNotFoundException {
+        Reader reader = new Reader();
+        SVGPath svgPath = new SVGPath();
+        svgPath.setContent(reader.readString(Constants.BOSS_PEDOBEAR_HITBOX));
+        super.setSvgPath(svgPath);
+    }
+
+    private void setRandomY(double randomY) {
+        this.randomY = randomY;
+    }
+
+    private double getRandomY() {
+        return randomY;
+    }
+
+    private Timer getTimer() {
+        return timer;
+    }
+
+    private void moveUp() {
+        this.setPositionY(this.getPositionY() - this.getSpeed());
+    }
+
+    private void moveDown() {
+        this.setPositionY(this.getPositionY() + this.getSpeed());
+    }
+
+    @Override
+    public void move() {
+        Random random = new Random();
+        Pedobear pedobear = this;
+
+        if (this.getPositionY() > this.getRandomY()) {
+            pedobear.moveUp();
+            return;
+        }
+        this.setRandomY(random.nextInt(20000));
+        if (this.getPositionY() < this.getRandomY()) {
+            pedobear.moveDown();
+            return;
+        }
+    }
 }
